e0acf589bc10e8c28a916de70e2dd4d1df47b1fd
Johann Mosin <89855069+AlataO@users.noreply.github.com>
2023-02-14 14:09:10 +0300

    Added more effective alert

--- fwc.user.js
+++ fwc.user.js
@@ -5,17 +5,20 @@
 // @description  Нахуй Крыла, скрипт будет обновляться
 // @author       Johann Mosin
 // @match        https://edu.vsu.ru/mod/attendance/view.php?id=1098103*
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Check for the text "Отметить" every 10 seconds
-    setInterval(function() {
+    var intervalId = setInterval(function() {
         if (document.body.textContent.includes('Отметить')) {
             // If the text is found, send an alert to the browser
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
             alert('Крыл охуел и решил проверить посещяемость!');
+            clearInterval(intervalId);
         } else {
             // If the text is not found, reload the page
             location.reload();
